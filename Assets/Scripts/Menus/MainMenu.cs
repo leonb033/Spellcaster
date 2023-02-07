a434fca2da373d6904447dce6453b02dad7be711
Cristina <c.nichita@web.de>
2023-02-07 13:58:24 +0100

    added fairy, questionmark, schloss, tip window

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 {
     public void NewGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Level1");
     }
 
     public void ContinueGame()
