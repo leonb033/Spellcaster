411482ef047ad4752fd75abe01784ba5d213e9e6
Cristina <c.nichita@web.de>
2023-03-10 17:44:48 +0100

    Story update, animation update

--- Assets/Scripts/Vocabulary/VocabularyMenu.cs
+++ Assets/Scripts/Vocabulary/VocabularyMenu.cs
@@ -6,14 +6,17 @@ using UnityEngine.UI;
 public class VocabularyMenu : MonoBehaviour
 {
     Manager manager;
+    GameObject voc_manager;
 
     void Start()
     {
+        voc_manager = GameObject.Find("/Vocabulary Manager");
         manager = GameObject.Find("/Manager").GetComponent<Manager>();
     }
 
     public void BackToLections()
     {
+        Destroy(voc_manager);
         manager.LoadScene("MainMenu");
     }
 
